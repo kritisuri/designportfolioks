568a7da2718b9f734af706defdb817fc66e3f486
Kriti Suri <kritisuri0931@outlook.com>
2023-05-28 09:48:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scrolling to "Design Portfolio" when image5 is clicked
+document.getElementById('image5').addEventListener('click', function() {
+    document.getElementById('text1').scrollIntoView({ behavior: 'smooth' });
+  });
+  
+  // Smooth scrolling to "C:\Users\Kriti Suri\Downloads\Frame 2 (1).png" when image7 is clicked
+  document.getElementById('image7').addEventListener('click', function() {
+    document.getElementById('image6').scrollIntoView({ behavior: 'smooth' });
+  });
+  
+  document.getElementById('scroll-link3').addEventListener('click', function() {
+    document.getElementById('image-5').scrollIntoView({ behavior: 'smooth' });
+  });
+  
