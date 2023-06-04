3c1a1d6c3f2494365f465830ebef6ea962404e51
Kriti Suri <kritisuri0931@outlook.com>
2023-06-05 00:13:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,23 +11,3 @@ document.getElementById('image5').addEventListener('click', function() {
   document.getElementById('scroll-link3').addEventListener('click', function() {
     document.getElementById('image-5').scrollIntoView({ behavior: 'smooth' });
   });
-  const nav = document.querySelector('nav');
-const toggle_btn = document.getElementById('toggle-btn');
-const content = document.querySelector('section');
-
-toggle_btn.onclick = function() {
-    nav.classList.toggle('hide');
-    content.classList.toggle('expand');
-};
-
-const body = document.querySelector('body');
-const bgModeBtn = document.getElementById('bgModeBtn');
-const bgModeIcon = document.getElementById('bgModeIcon');
-const sectionHeader = document.querySelector('section h1');
-
-bgModeBtn.onclick = function(){
-    body.classList.toggle("dark-mode");
-    bgModeIcon.classList.toggle("fa-sun-o");
-    bgModeIcon.classList.toggle("fa-moon-o");
-    sectionHeader.classList.toggle("dark-mode");
-}
